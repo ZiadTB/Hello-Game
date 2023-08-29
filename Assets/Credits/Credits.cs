9a606b7996b062df830aa40705abc75efa491954
ZiadTB <ziad.ztlb@gmail.com>
2023-08-29 22:39:10 +0300

    Hello Fix 2

---
+++ Assets/Credits/Credits.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public Animator transition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevel(0));
+
+        IEnumerator LoadLevel(int levelIndex)
+        {
+            transition.SetTrigger("Start");
+
+            yield return new WaitForSeconds(16);
+
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/ToCredits.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToCredits : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("TRIGGER ENTERED!!!");
+        if (collision.tag == "Player")
+        {
+            Debug.Log("COLLISION BITCHES");
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
